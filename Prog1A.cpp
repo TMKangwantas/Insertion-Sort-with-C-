b6e7ba48e354377def92985ed8b187ec5839f33d
TMKangwantas <tmkangwantas@gmail.com>
2016-03-18 20:09:39 -0400

    Added files via upload

---
+++ Prog1A.cpp
@@ -0,0 +1,119 @@
+/*
+ * Prog1.cpp
+ *
+ *  Created on: Mar 18, 2016
+ *      Author: Mark
+ */
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
+
+bool smaller (int A[], int x, int y)
+{
+	if (A[x] < A[y])
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	int COMPCOUNT1 = 0; //The number of key comparisons for the worst case
+	int COMPCOUNT2 = 0; //The number of key comparisons for the average case
+	int insort1[32]; //Best Case
+	int insort2[6]; //Worst Case
+	int insort3[32]; //Average Case
+
+
+	cout << "Best Case" << endl;
+	int best = 0;
+	for (int i = 0; i < 32; i++)
+	{
+		insort1[i] = best;
+		best++;
+	}
+	//Comparison not needed since the array is already sorted
+	//Input and output array will be the same
+	cout << "Input and Output Array: " << endl;
+	for (int i = 0; i < 32; i++)
+		cout << insort1[i] << " ";
+	cout << endl;
+	cout << "The number of key comparisons is 0" << endl; //0 since everything is sorted already
+
+	cout << "\n" << "\n";
+
+	cout << "Worst Case" << endl;
+
+	int rando;
+	for (int i = 0; i < 32; i++){
+			insort2[i] = best;
+			best--;
+		}
+	cout << "The input array: " << endl;
+		for (int i = 0; i < 32; i++)
+			cout << insort2[i] << " ";
+		cout << endl;
+
+
+	for (int j = 0; j < 31; j++)
+	{
+			for (int i = j+1; i > 0; i--)
+			{
+
+				if (smaller(insort2, i, (i-1) ))
+				{
+					COMPCOUNT1++;
+					//Swap the elements
+					int temp = insort2[i];
+					insort2[i] = insort2[i-1];
+					insort2[i-1] = temp;
+				}
+			}
+		}
+
+	cout << "The output array: " << endl;
+		for (int i = 0; i < 32; i++)
+			cout << insort2[i] << " ";
+
+	cout << endl << "The number of key comparisons is: " << COMPCOUNT1 << endl;
+
+	cout << "\n" << "\n";
+
+	cout << "Average Case" << endl;
+	srand(time(NULL));
+	for (int i = 0; i < 32; i++)
+	{
+		rando = rand() % 30;
+		insort3[i] = rando;
+	}
+
+	cout << "The input array: " << endl;
+	for (int i = 0; i < 32; i++)
+		cout << insort3[i] << " ";
+	cout << endl;
+
+
+	for (int j = 0; j < 31; j++)
+	{
+		for (int i = j+1; i > 0; i--)
+		{
+			if (smaller(insort3, i, (i-1) ))
+			{
+				COMPCOUNT2++;
+
+				//Swap the elements
+				int temp = insort3[i];
+				insort3[i] = insort3[i-1];
+				insort3[i-1] = temp;
+			}
+		}
+	}
+
+	cout << "The output array: " << endl;
+	for (int i = 0; i < 32; i++)
+		cout << insort3[i] << " ";
+
+	cout << endl << "The number of key comparisons is: " << COMPCOUNT2 << endl;
+}
